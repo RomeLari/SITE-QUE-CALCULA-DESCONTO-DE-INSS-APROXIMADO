552a1a3c3ad5fd9e758733b666efac27632c4417
Larissa Romero <larissaromero@terra.com.br>
2026-09-07 19:42:30 -0300

    VAMOS VER SE ESTÁ FUNCIONANDO

--- app.py
+++ app.py
@@ -3,30 +3,46 @@ from flask import Flask, render_template, request
 salario_bruto = float(input("Digite o seu salário bruto:"))
 
 app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
 
-if salario_bruto <= 1621 :
-    inss = salario_bruto * 75/1000
+def home():
 
-elif 1621 < salario_bruto < 2902 :
-    faixa_1 = 1621 * 75/1000
-    faixa_2 = (salario_bruto - 1621) * 9/100
+    #Se o método for igual a POST, faça isso
+    #Isso já liga a informação do html com o python
 
-    inss = faixa_1 + faixa_2
+    if request.method == "POST":
 
-elif 2902 < salario_bruto <= 4354 :
-    faixa_1 = 1621 * 75/1000
-    faixa_2 = (2902-1621) * 9/100
-    faixa_3 = (salario_bruto - 2902) * 12/100
 
-    inss = faixa_1 + faixa_2 + faixa_3
+        if salario_bruto <= 1621 :
+            inss = salario_bruto * 75/1000
 
-elif 4354 < salario_bruto <= 8475 :
-    faixa_1 = 1621 * 75/1000
-    faixa_2 = (2902-1621) * 9/100
-    faixa_3 = (4354-2902) * 12/100
-    faixa_4 = (salario_bruto-4354) * 14/100
+        elif 1621 < salario_bruto < 2902 :
+            faixa_1 = 1621 * 75/1000
+            faixa_2 = (salario_bruto - 1621) * 9/100
 
-    inss = faixa_1 + faixa_2 + faixa_3 + faixa_4 
+            inss = faixa_1 + faixa_2
 
-inss = round(inss, 2)
-print("INSS aproximado:", inss)
+        elif 2902 < salario_bruto <= 4354 :
+            faixa_1 = 1621 * 75/1000
+            faixa_2 = (2902-1621) * 9/100
+            faixa_3 = (salario_bruto - 2902) * 12/100
+
+            inss = faixa_1 + faixa_2 + faixa_3
+
+        elif 4354 < salario_bruto <= 8475 :
+            faixa_1 = 1621 * 75/1000
+            faixa_2 = (2902-1621) * 9/100
+            faixa_3 = (4354-2902) * 12/100
+            faixa_4 = (salario_bruto-4354) * 14/100
+
+            inss = faixa_1 + faixa_2 + faixa_3 + faixa_4 
+
+        inss = round(inss, 2)
+
+        return render_template("index.html", salario_bruto=salario_bruto, inss=inss)
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
